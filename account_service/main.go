291a6523f941c57796278c833bf8e7f87b7cb884
JorgeAlvarez <joregebadlibre@gmail.com>
2024-10-07 23:20:25 -0500

    message

--- account_service/main.go
+++ account_service/main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/streadway/amqp"
 )
 
 type Cuenta struct {
@@ -21,4 +23,54 @@ func main() {
 
 	log.Println("Cuenta service running on port 8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func receiveMessages() {
+	conn, err := amqp.Dial("RABBITMQ_URL")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"persona_queue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+}
+
+func main() {
+	go receiveMessages()
+	// Resto del código...
 }
--- go.mod
+++ go.mod
@@ -2,5 +2,4 @@ module github.com/joregebadlibre/goMicroService
 
 go 1.23.2
 
-
 require github.com/streadway/amqp v1.1.0 // indirect
